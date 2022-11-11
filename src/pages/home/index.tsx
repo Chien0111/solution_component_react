bb1bffa372757a92d99fc626905e4f04dfb55799
xuanchien0111 <chiennx@hocmai.vn>
2022-11-11 16:59:56 +0700

    add home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { RingProgress, Text } from "@mantine/core";
+import CardLineChart from "../../componets/element/CardLineChart";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="flex flex-wrap w-full">
+        <div>
+          <CardLineChart />
+          <CardLineChart />
+        </div>
+        <div>
+          <RingProgress
+            label={
+              <Text size="xs" align="center">
+                Application data usage
+              </Text>
+            }
+            sections={[
+              { value: 40, color: "cyan" },
+              { value: 15, color: "orange" },
+              { value: 15, color: "grape" },
+            ]}
+          />
+        </div>
+        <div className="w-full xl:w-4/12 px-4"></div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
